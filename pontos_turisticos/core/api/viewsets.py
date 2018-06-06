28ecbc2aec4bd09acd52137beb65268f6611e550
hunk <agenthunk1998@gmail.com>
2018-06-06 17:26:59 -0300

    implementando imagens

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -13,22 +13,23 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
-        return Response({'teste': 123})
+        """return Response({'teste': 123})"""
+        return super(PontoTuristicoViewSet, self).list(self, request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-        return Response({'Olaaaaaaaa': request.data['nome']})
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).partial_update(self, request, *args, **kwargs)
 
     @action(methods=['post', 'post'], detail=True)
     def denunciar(self, request, pk=None):
--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from endereco.api.viewsets import EnderecoViewSet
@@ -33,4 +35,4 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
