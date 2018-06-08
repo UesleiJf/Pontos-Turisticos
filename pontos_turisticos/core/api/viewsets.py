b5085691ca0eede3c2c9f63cb9faae5986b265f4
hunk <agenthunk1998@gmail.com>
2018-06-08 13:17:54 -0300

    utilizando o search field

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -8,6 +9,8 @@ from .serializers import PontoTuristicoSerializer
 class PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('nome', 'descricao', 'endereco__linha1')
 
     def get_queryset(self):
         """"return PontoTuristico.objects.filter(aprovado=True)"""
