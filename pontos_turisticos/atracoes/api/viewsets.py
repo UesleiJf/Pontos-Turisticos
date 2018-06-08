de032ee65ae77f6edd575b4798fe648f37ae6b0a
hunk <agenthunk1998@gmail.com>
2018-06-08 12:59:59 -0300

    utilizando django filter backend

--- pontos_turisticos/atracoes/api/viewsets.py
+++ pontos_turisticos/atracoes/api/viewsets.py
@@ -6,3 +6,5 @@ from .serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+
+    filter_fields = ('nome', 'descricao')
--- pontos_turisticos/pontos_turisticos/settings.py
+++ pontos_turisticos/pontos_turisticos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'core',
     'atracoes',
     'comentarios',
@@ -128,3 +129,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
