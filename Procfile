5dd79b0d955c791a461a28154b7c6863ad0c3466
hunk <agenthunk1998@gmail.com>
2018-06-11 11:55:59 -0300

    configure Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn pontos-turisticos-2018.wsgi --log-file -
+web: gunicorn pontos_turisticos.wsgi --log-file -
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos-2018.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['pontosturisticos2018.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
